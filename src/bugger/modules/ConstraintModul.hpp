3030be01c716ae580b5c1cedf8b0ded100dd5641
Dominik Kamp <dominik.kamp@uni-bayreuth.de>
2023-12-27 14:08:06 +0100

    Resolve constraint pairing

--- src/bugger/modules/ConstraintModul.hpp
+++ src/bugger/modules/ConstraintModul.hpp
@@ -60,8 +60,6 @@ namespace bugger {
       execute(Problem<double> &problem, Solution<double> &solution, bool solution_exists, const BuggerOptions &options,
               const Timer &timer) override {
 
-         ModulStatus result = ModulStatus::kUnsuccesful;
-
          auto copy = Problem<double>(problem);
          Vec<int> applied_redundant_rows { };
          Vec<int> batches { };
@@ -77,59 +75,44 @@ namespace bugger {
             batchsize /= options.nbatches;
          }
 
-         int nbatch = 0;
+         batches.reserve(batchsize);
 
          for( int row = copy.getNRows( ) - 1; row >= 0; --row )
          {
-            batches.clear();
-            batches.reserve(batchsize);
-            if( isConstraintAdmissible(copy, row))
+            if( isConstraintAdmissible(copy, row) )
             {
                assert(!copy.getRowFlags( )[ row ].test(RowFlag::kRedundant));
                copy.getRowFlags( )[ row ].set(RowFlag::kRedundant);
-               assert(copy.getRowFlags( )[ row ].test(RowFlag::kRedundant));
-
                batches.push_back(row);
-
-               if( nbatch >= 1 && ( nbatch >= batchsize || row <= 0 ))
-               {
-                  auto solver = createSolver();
-                  solver->parseParameters();
-                  solver->doSetUp(copy, solution_exists, solution);
-                  if( solver->run(msg,originalSolverStatus) != BuggerStatus::kFail )
-                  {
-                     copy = Problem<double>(problem);
-                     for( const auto &item: applied_redundant_rows )
-                     {
-                        assert(!copy.getRowFlags( )[ item ].test(RowFlag::kRedundant));
-                        copy.getRowFlags( )[ item ].set(RowFlag::kRedundant);
-                     }
-                  }
-                  else
-                  {
-                     //TODO: push back together
-                     for( const auto &item: batches )
-                        applied_redundant_rows.push_back(item);
-                     ndeletedrows += nbatch;
-                     result = ModulStatus::kSuccessful;
-
-                     auto copy2 = Problem<double>(problem);
-                     for( const auto &item: applied_redundant_rows )
-                     {
-                        assert(!copy2.getRowFlags( )[ item ].test(RowFlag::kRedundant));
-                        copy2.getRowFlags( )[ item ].set(RowFlag::kRedundant);
-                     }
-                  }
-                  nbatch = 0;
-               }
-               ++nbatch;
             }
 
+            if( batches.size() >= 1 && ( batches.size() >= batchsize || row <= 0 ) )
+            {
+               auto solver = createSolver();
+               solver->parseParameters();
+               solver->doSetUp(copy, solution_exists, solution);
+               if( solver->run(msg, originalSolverStatus) != BuggerStatus::kFail )
+               {
+                  copy = Problem<double>(problem);
+                  for( const auto &item: applied_redundant_rows )
+                  {
+                     assert(!copy.getRowFlags( )[ item ].test(RowFlag::kRedundant));
+                     copy.getRowFlags( )[ item ].set(RowFlag::kRedundant);
+                  }
+               }
+               else
+               {
+                  applied_redundant_rows.insert(applied_redundant_rows.end(), batches.begin(), batches.end());
+               }
+               batches.clear();
+            }
          }
 
-         problem = Problem<double>(copy);
          //TODO: remove the constraints from the problem might be ideal at least at the end
-         return result;
+         problem = Problem<double>(copy);
+         ndeletedrows += applied_redundant_rows.size();
+
+         return applied_redundant_rows.size() > 0 ? ModulStatus::kSuccessful : ModulStatus::kUnsuccesful;
       }
    };
 
