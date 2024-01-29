c52d8ef1fc53e4b6a5572c641053a95f6966aa34
Dominik Kamp <dominik.kamp@uni-bayreuth.de>
2024-01-29 17:45:07 +0100

    Fix consround success

--- src/bugger/modules/ConsroundModul.hpp
+++ src/bugger/modules/ConsroundModul.hpp
@@ -183,7 +183,7 @@ namespace bugger {
 
          if(!admissible)
             return ModulStatus::kNotAdmissible;
-         if( applied_entries.empty() && applied_reductions_lhs.empty() || !applied_reductions_rhs.empty() )
+         if( applied_entries.empty() && applied_reductions_lhs.empty() && applied_reductions_rhs.empty() )
             return ModulStatus::kUnsuccesful;
          else
          {
