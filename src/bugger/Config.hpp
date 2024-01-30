4b2cf1a0c329ee3041aa7ba6e10b85908ad5d6e3
Alexander Hoen <hoen@zib.de>
2024-01-30 09:35:41 +0100

    change Copyright

--- src/bugger/Config.hpp
+++ src/bugger/Config.hpp
@@ -3,8 +3,7 @@
 /*               This file is part of the program and library                */
 /*    BUGGER --- Parallel Presolve for Integer and Linear Optimization       */
 /*                                                                           */
-/* Copyright (C) 2023             Konrad-Zuse-Zentrum                        */
-/*                     fuer Informationstechnik Berlin                       */
+/* Copyright (C) 2024             Zuse Institute Berlin                           */
 /*                                                                           */
 /* This program is free software: you can redistribute it and/or modify      */
 /* it under the terms of the GNU Lesser General Public License as published  */
